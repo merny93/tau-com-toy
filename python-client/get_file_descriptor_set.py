85f31b181da05004986c65a63108714b64add5eb
Steven Benton <sbenton@princeton.edu>
2024-11-19 08:10:33 -0500

    parse with message_factory rather than descriptor_pool

--- python-client/get_file_descriptor_set.py
+++ python-client/get_file_descriptor_set.py
@@ -1,6 +1,6 @@
 import socket
 import time
-from google.protobuf import descriptor_pool, descriptor_pb2
+from google.protobuf import descriptor_pb2, message_factory
 
 def get_info(socket_path):
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
@@ -15,16 +15,14 @@ def parse_descriptors(descriptors):
 
     fd_set = descriptor_pb2.FileDescriptorSet()
     fd_set.ParseFromString(descriptors)
-    print(fd_set)  # this is big
+    # print(fd_set)  # this is big
 
-    # it's not clear to me the pool is more useful than the set
-    pool = descriptor_pool.DescriptorPool()
-    for fd in fd_set.file:
-        pool.Add(fd)
+    message_classes = message_factory.GetMessages(fd_set.file)
+    return fd_set, message_classes
 
 
 if __name__ == "__main__":
     descriptors = get_info("../info_socket")
-    # with open("descriptors.dat", "wb") as f:
-    #     f.write(descriptors)
-    parse_descriptors(descriptors)
+    fd_set, message_classes = parse_descriptors(descriptors)
+    print("Known message types:")
+    print(message_classes.keys())
