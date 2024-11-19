03508caec533249a3c3ea85e66eb01145e71cc6b
Steven Benton <sbenton@princeton.edu>
2024-11-18 20:53:15 -0500

    parse FileDescriptorSet in python

--- python-client/get_file_descriptor_set.py
+++ python-client/get_file_descriptor_set.py
@@ -1,6 +1,6 @@
 import socket
 import time
-
+from google.protobuf import descriptor_pool, descriptor_pb2
 
 def get_info(socket_path):
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
@@ -11,5 +11,20 @@ def get_info(socket_path):
         return response
 
 
+def parse_descriptors(descriptors):
+
+    fd_set = descriptor_pb2.FileDescriptorSet()
+    fd_set.ParseFromString(descriptors)
+    print(fd_set)  # this is big
+
+    # it's not clear to me the pool is more useful than the set
+    pool = descriptor_pool.DescriptorPool()
+    for fd in fd_set.file:
+        pool.Add(fd)
+
+
 if __name__ == "__main__":
-    get_info("../info_socket")
+    descriptors = get_info("../info_socket")
+    # with open("descriptors.dat", "wb") as f:
+    #     f.write(descriptors)
+    parse_descriptors(descriptors)
